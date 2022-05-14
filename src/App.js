29f56ddfa67ddca9e2d1c6b1347bfffd64062c26
avnit007 <avnitnavadiya100@gmail.com>
2022-05-14 19:47:10 +0530

    feat: redux and redux thunk

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React , { useEffect, useState } from 'react'
 import Banner from "./components/Banner";
 import Blog from "./components/Blog";
 import { DealOfTheWeek } from "./components/DealOfTheWeek";
@@ -8,22 +9,49 @@ import InstagramPhotoes from "./components/InstagaramPhotoes";
 import ManBanner from "./components/ManBanner";
 import WomenBanner from "./components/WomenBanner";
 import PartnerLogo from "./PartnerLogoes";
+import axios from "axios";
 
-function App() {
+
+import { useSelector } from "react-redux";
+import store from "./store";
+
+
+import { loadUser } from "./actions/userAction";
+
+
+const App = () => {
+
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/v1/stripeapikey");
+
+    setStripeApiKey(data.stripeApiKey);
+  }
+
+  useEffect(() => {
+
+    store.dispatch(loadUser());
+
+    getStripeApiKey();
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Heroes />
-      <Banner />
-      <WomenBanner />
-      <DealOfTheWeek />
-      <ManBanner />
-      <InstagramPhotoes />
-      <Blog />
-      <PartnerLogo />
-      <FooterSection />
-    </div>
-  );
+    <Header />
+    <Heroes />
+    <Banner />
+    <WomenBanner />
+    <DealOfTheWeek />
+    <ManBanner />
+    <InstagramPhotoes />
+    <Blog />
+    <PartnerLogo />
+    <FooterSection />
+  </div>
+  )
 }
 
-export default App;
+export default App
